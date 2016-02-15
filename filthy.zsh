e2124ec7020ab193725b84fb70b3a46679b9067b
James Dinsdale <hi@molovo.co>
2016-02-15 12:30:13 +0000

    Add nice exit codes as an option

--- filthy.zsh
+++ filthy.zsh
@@ -18,6 +18,49 @@
 # %m => shortname host
 # %(?..) => prompt conditional - %(condition.true.false)
 
+prompt_filthy_nice_exit_code() {
+	local exit_status="${1:-$(print -P %?)}";
+	# nothing to do here
+	[[ ${FILTHY_SHOW_EXIT_CODE:=0} != 1 || -z $exit_status || $exit_status == 0 ]] && return;
+
+	local sig_name;
+
+	# is this a signal name (error code = signal + 128) ?
+	case $exit_status in
+		129)  sig_name=HUP ;;
+		130)  sig_name=INT ;;
+		131)  sig_name=QUIT ;;
+		132)  sig_name=ILL ;;
+		134)  sig_name=ABRT ;;
+		136)  sig_name=FPE ;;
+		137)  sig_name=KILL ;;
+		139)  sig_name=SEGV ;;
+		141)  sig_name=PIPE ;;
+		143)  sig_name=TERM ;;
+	esac
+
+	# usual exit codes
+	case $exit_status in
+		-1)   sig_name=FATAL ;;
+		1)    sig_name=WARN ;; # Miscellaneous errors, such as "divide by zero"
+		2)    sig_name=BUILTINMISUSE ;; # misuse of shell builtins (pretty rare)
+		126)  sig_name=CCANNOTINVOKE ;; # cannot invoke requested command (ex : source script_with_syntax_error)
+		127)  sig_name=CNOTFOUND ;; # command not found (ex : source script_not_existing)
+	esac
+
+	# assuming we are on an x86 system here
+	# this MIGHT get annoying since those are in a range of exit codes
+	# programs sometimes use.... we'll see.
+	case $exit_status in
+		19)  sig_name=STOP ;;
+		20)  sig_name=TSTP ;;
+		21)  sig_name=TTIN ;;
+		22)  sig_name=TTOU ;;
+	esac
+
+	echo "$ZSH_PROMPT_EXIT_SIGNAL_PREFIX${exit_status}:${sig_name:-$exit_status}$ZSH_PROMPT_EXIT_SIGNAL_SUFFIX ";
+}
+
 # turns seconds into human readable time
 # 165392 => 1d 21h 56m 32s
 prompt_filthy_human_time() {
@@ -167,7 +210,7 @@ prompt_filthy_setup() {
   RPROMPT='%(!.%F{red}%n%f%F{242}.%F{green}%n%f%F{242})@%m%f'
 
   # prompt turns red if the previous command didn't exit with 0
-  PROMPT='%(?.%F{green}.%F{red})❯%f '
+  PROMPT='%(?.%F{green}.%F{red}$(prompt_filthy_nice_exit_code))❯%f '
 }
 
 prompt_filthy_setup "$@"
